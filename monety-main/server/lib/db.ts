0e5f521d61a97150f25f7f2b50e92065bc63ad22
lippexclash-sys <lippexclash@gmail.com>
2026-03-14 21:16:51 -0300

    Delete monety-main directory

--- monety-main/server/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { IdealaneDB } from '@idealane/node-sdk';
-
-export const db = new IdealaneDB({
-  connectionString: process.env.DATABASE_URL!,
-  ssl: { rejectUnauthorized: false }
-});
-
-export const isDbReady = (): boolean => !!process.env.DATABASE_URL;
